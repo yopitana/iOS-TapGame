8c321e64a4d67489d555f1a704eef11c1b070824
Yoshiyuki.tanaka <yopi0928@gmail.com>
2012-10-20 02:00:41 +0900

    initialize project.

---
+++ TapGame/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  TapGame
+//
+//  Created by 慶之 田中 on 12/10/20.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ TapGame/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  TapGame
+//
+//  Created by 慶之 田中 on 12/10/20.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
